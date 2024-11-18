0fd0dae12b5aced44a120473acd198ab6fe714bf
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2024-11-17 21:52:07 -0500

    Update nonograms.ml

--- nonograms.ml
+++ nonograms.ml
@@ -8,6 +8,44 @@ type nonogram = row list
 
 type run_range = { start_pos : int; end_pos : int; length : int }
 
+type cell = Black | White | Unknown 
+type row = cell list
+type grid = row list 
+                     
+let split index grid =
+  let rec aux i acc rest =
+    match rest with
+    | [] -> (List.rev acc, [])
+    | h :: t ->
+        if i = 0 then (List.rev acc, rest)
+        else aux (i - 1) (h :: acc) t
+  in
+  aux index [] grid
+;;
+
+let rec verify_col (grid: grid) (clues: int list) i =
+  match clues with 
+  | [] -> List.for_all (fun x -> (List.nth x i = White || List.nth x i = Unknown)) grid
+  | h::t -> 
+      if List.nth (List.hd grid) i = Black then 
+        let (firstk, rest) = split h grid in 
+        if List.for_all (fun x -> (List.nth x i = Black || List.nth x i = Unknown)) (firstk) then ( 
+          match rest with 
+          | [] -> (clues = [])
+          | r::rows -> if List.nth r i = White || List.nth r i = Unknown then verify_col rows t i else false
+        ) else false 
+      else if List.nth (List.hd grid) i = White then let r::rows = grid in verify_col rows clues i
+      else
+        (
+          let (firstk, rest) = split h grid in 
+          if List.for_all (fun x -> (List.nth x i = Black || List.nth x i = Unknown)) (firstk) then ( 
+            match rest with 
+            | [] -> (clues = [])
+            | r::rows -> if List.nth r i  = White || List.nth r i  = Unknown then verify_col rows t i else false
+          ) else false
+        ) || let r::rows = grid in verify_col rows clues i
+;;
+
 (* A helper function to initialize a grid with unknown cells *)
 let initialize_grid rows cols =
   Array.make_matrix rows cols Unknown
