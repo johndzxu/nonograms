7cf74693035d88b31f0f872ff029a4158f29110c
John Hsu <77399093+johndzxu@users.noreply.github.com>
2024-11-19 22:41:01 -0500

    add rule application functions

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,11 +1,5 @@
 open Rules
 
-type cell = Black | White | Unknown 
-type row = cell list
-type grid = row list
-
-type run_range = { start_pos : int; end_pos : int; length : int }
-
 let split index grid =
   let rec aux i acc rest =
     match rest with
@@ -79,25 +73,76 @@ let update_row row index value =
 
 
 (* Apply all logical rules iteratively *)
-let apply_logical_rules grid run_ranges =
-  let rec iterate grid =
-    (* Apply all rules and determine changes *)
-    let new_grid = grid in
-    (* Apply each rule in sequence *)
-    let new_grid = rule_1_1 run_ranges new_grid in
-    let new_grid = rule_1_2 run_ranges new_grid in
-    let new_grid = rule_1_3 run_ranges new_grid in
-    let new_grid = rule_1_4 run_ranges new_grid in
-    let new_grid = rule_1_5_part1 run_ranges new_grid in
-    let new_grid = rule_1_5_part2 run_ranges new_grid in
-    let run_ranges = rule_2_1 run_ranges in
-    let run_ranges = rule_2_2 run_ranges new_grid in
-    let run_ranges = rule_2_3 run_ranges new_grid in
-    let (run_ranges, new_grid) = rule_3_1 run_ranges new_grid in
-    let run_ranges = rule_3_2 run_ranges new_grid in
-    let new_grid = rule_3_3 run_ranges new_grid in
-    if new_grid = grid then grid else iterate new_grid
+(* Function to initialize run ranges based on the row and clues *)
+let initialize_run_ranges row row_clues =
+  let n = List.length row in
+  let k = List.length row_clues in
+
+  (* Helper function to calculate the sum of (LB_i + 1) for given range *)
+  let sum_clues_with_gaps clues from_idx to_idx =
+    List.fold_left (fun acc i -> acc + List.nth clues i + 1) 0 (list_init (to_idx - from_idx + 1) (fun i -> from_idx + i))
   in
+
+  (* Compute each run's start and end positions *)
+  List.mapi (fun j lbj ->
+    let start_pos =
+      if j = 0 then 0
+      else sum_clues_with_gaps row_clues 0 (j - 1)
+    in
+    let end_pos =
+      if j = k - 1 then n - 1
+      else (n - 1) - sum_clues_with_gaps row_clues (j + 1) (k - 1)
+    in
+    { start_pos; end_pos; length = lbj }
+  ) row_clues
+
+
+
+let apply_rules_row row run_ranges =
+  let rec iterate row run_ranges =
+    (* Apply each rule in sequence *)
+    let new_row = rule_1_1 run_ranges row in
+    let new_row = rule_1_2 run_ranges new_row in
+    let new_row = rule_1_3 run_ranges new_row in
+    let new_row = rule_1_4 run_ranges new_row in
+    let new_row = rule_1_5_part1 run_ranges new_row in
+    let new_row = rule_1_5_part2 run_ranges new_row in
+    let new_run_ranges = rule_2_1 run_ranges in
+    (*let new_run_ranges = rule_2_2 new_run_ranges new_row in *)
+    let new_run_ranges = rule_2_3 new_run_ranges new_row in
+    let (new_run_ranges, new_row) = rule_3_1 new_run_ranges new_row in
+  (*  let run_ranges = rule_3_2 run_ranges new_row in
+    let new_row = rule_3_3 run_ranges new_row in *)
+    if new_row = row && new_run_ranges = run_ranges
+      then new_row
+      else iterate new_row new_run_ranges
+  in
+  iterate row run_ranges
+
+let apply_rules_rows (grid: row list) (clues: int list list): grid = 
+  let run_ranges_grid = (List.map2 initialize_run_ranges grid clues) in
+  let new_grid = (List.map2 apply_rules_row grid run_ranges_grid) in
+  new_grid
+
+let gen_list n e =
+  let rec gen' n cont = 
+    match n with
+    | 0 -> cont []
+    | _ -> gen' (n-1) (fun l -> cont (e::l)) in gen' n (fun a -> a) 
+    
+let transpose nono = 
+  let len = List.length (List.hd nono) in
+  let cols = List.fold_left (fun cols row -> List.map2 (fun col square -> (square::col)) cols row) (gen_list len []) nono
+  in List.map (fun l -> List.rev l) cols
+
+let apply_logical_rules (grid: row list) (row_clues: int list list) (col_clues: int list list): grid =
+  let rec iterate grid =
+  let new_grid = apply_rules_rows grid row_clues in
+  let new_grid = transpose new_grid in
+  let new_grid = apply_rules_rows new_grid col_clues in
+  let new_grid = transpose new_grid in
+  if new_grid = grid then grid else (iterate new_grid) in
+
   iterate grid
 
 (* Backtracking function placeholder *)
@@ -108,3 +153,14 @@ let backtrack grid run_ranges =
 let solve_nonogram grid run_ranges =
   let logical_solved = apply_logical_rules grid run_ranges in
   backtrack logical_solved run_ranges
+
+
+let grid =
+  [[Unknown; Unknown; Unknown; Unknown];
+   [Unknown; Unknown; Unknown; Unknown];
+   [Unknown; Unknown; Unknown; Unknown];
+   [Unknown; Unknown; Unknown; Unknown]];;
+let row_clues = [[1]; [1; 2]; [2; 1]; [1; 1]];;
+let col_clues = [[4]; [1]; [1]; [3]];;
+apply_logical_rules grid row_clues col_clues ;;
+
