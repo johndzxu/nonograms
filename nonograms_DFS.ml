6964eaed16cf83c02fcbaecc0cda9fc7e05a0000
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2024-11-19 20:38:46 -0500

    working on getting backtracking finished with logical rules

--- nonograms_DFS.ml
+++ nonograms_DFS.ml
@@ -49,77 +49,21 @@ let rec ver_grid nono clues =
   let clue_cols = List.map2 (fun clue col -> (clue,col)) clues (transpose nono) in
   List.for_all (fun (clue, col) -> ver_col clue col Unknown) clue_cols
   
-(* Solve with Exceptions and Backtracking *)
-(* Assume you have access to a function that uses the 11 rules for solving nonograms and applies them recursively until there are no more changes, and then a function that fills in gray squares to generate all posible new rows *)
-let solve row_cls col_cls =  
-  let rec solve' nono row_cls col_cls = 
-    if ver_grid nono col_cls then nono else
-      let new_rows = all_rows row_cls (List.length (List.hd nono)) in
-      let new_nono = List.map (fun row -> row::nono) new_rows in
-      let rec solve'' nono = 
-        match nono with
-        | [] -> raise Fail
-        | n::ns -> if ver_grid n col_cls then n else solve'' ns in
-      solve'' (List.map (fun n -> solve' n row_cls col_cls) new_nono) in
-  solve' [] row_cls col_cls  
-;;
-                     
-
-(* let rec s_row row_cls col_cls nono = 
-    if ver_grid nono col_cls then 
-      let new_nono = apply_rules nono in 
-      let children = generate_children new_nono in 
-      try_children row_cls col_cls children
-    else raise Fail
-  and try_children row_cls col_cls children = 
-    match children with 
-    | [] -> raise Fail
-    | x::xs -> (
-        try s_row row_cls col_cls x 
-        with Fail -> s_stack row_cls col_cls nono xs
-      ) 
-
-;; *)
-
-
-let find_first_row_with_grays nono = 
-  let rec find' nono row_id = 
-    match nono with
-    | [] -> raise Fail
-    | x::xs -> if List.mem Unknown x then row_id else find' xs (row_id+1) in
-  find' nono 0
-;;
-
-let number_of_grays row_id nono = 
-  List.length (List.filter (fun x -> x = Unknown) (List.nth nono row_id))
-;;
-
-let rec binary_permutations n =
-  if n = 0 then [[]] (* Base case: single empty list when n = 0 *)
-  else
-    let smaller = binary_permutations (n - 1) in
-    (* Prepend 0 and 1 to all smaller permutations *)
-    List.map (fun l -> White :: l) smaller @ List.map (fun l -> Black :: l) smaller
-  ;;
-
-(* for a given row replace all the gray with the combo given and insert that into the nono  *)
-let replace_grays row_id combo nono =
-  let rec replace' row combo = 
-    match row with
-    | [] -> []
-    | x::xs -> if x = Unknown then (List.hd combo)::replace' xs (List.tl combo) else x::replace' xs combo in
-  let new_row = replace' (List.nth nono row_id) combo in
-  List.mapi (fun i r -> if i = row_id then new_row else r) nono
-  ;;
-
-(* let insert_rows child_rows nono row_id = 
-    List.mapi (fun i r -> if i = row_id then child_rows else r) nono
-  ;; *)
-
-let generate_children nono =
-    let row_id = find_first_row_with_grays nono in
-    let gray_combos = binary_permutations (number_of_grays row_id nono) in
-    List.map (fun combo -> replace_grays row_id combo nono) gray_combos
-    (* filter based on row and column clues? *)
-  ;;
+let solve row_cls col_cls =
+    let width = List.length col_cls in
+    
+    let rec s_row row_cls col_cls nono = 
+      match row_cls with 
+      | [] -> let nono = List.rev nono in 
+          if ver_grid nono col_cls then nono else raise Fail
+      | cl::cls -> s_stack row_cls col_cls nono (all_rows cl width) 
+    and s_stack row_cls col_cls nono stack = 
+      match (stack, row_cls) with 
+      | ([], _) -> raise Fail
+      | (x::xs, cl::oth_cls) ->
+          (try s_row oth_cls col_cls (x::nono) 
+           with Fail -> s_stack row_cls col_cls nono xs)
+      | _-> raise Fail
+              
+    in s_row row_cls col_cls []
   
